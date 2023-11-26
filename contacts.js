52c35609081e9cad7b57b8c7bfcf71ae17d06766
astasiempre <ero1494651@gmail.com>
2023-11-26 20:52:41 +0200

    first commit

---
+++ contacts.js
@@ -0,0 +1,33 @@
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+console.log(contactsPath);
+
+// const readFile = async () => {
+//     const data = await fs.readFile("./files/file.txt")
+//     console.log(data)
+// }
+
+// readFile()
+
+/*
+ * Розкоментуй і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту. 
+}
